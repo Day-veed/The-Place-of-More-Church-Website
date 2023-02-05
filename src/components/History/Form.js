6094040b5894f75022ac055cc5f1276906776685
Day-veed <davidadekunle9@gmail.com>
2023-02-05 14:25:51 +0100

    MTCformUpdate

--- src/components/History/Form.js
+++ src/components/History/Form.js
@@ -24,7 +24,10 @@ function Form() {
     //const [about, setAbout] = useState();
     //const [select, setSelect] = useState();
     const [file, setFile] = useState(null);
-    const [available, setAvailable] = useState('Yes');
+    const [available, setAvailable] = useState('');
+    const [taken, setTaken] = useState('');
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState('');
     
     const [attachmentUploading, setAtttachmentUploading] = useState(false);
 
@@ -70,6 +73,9 @@ function Form() {
           number,
           location,
           available,
+          taken,
+          month,
+          year,
           attachment: attachmentUrl
         }
     
@@ -80,7 +86,11 @@ function Form() {
         setNumber('')
         setLocation('')
         setAvailable('')
+        setTaken('')
+        setMonth('')
+        setYear('')
         setFile(null)
+        uncheckFunction()
           
     }, {
         pending: "Sending Email...",
@@ -93,6 +103,11 @@ function Form() {
    }
 
     };
+
+    const uncheckFunction = () => {
+        let allRadioButtons = document.querySelectorAll('.radioB');
+        allRadioButtons.forEach(value=> value.checked = false);
+    }
   
     const uploadFiles = async (file) => {
         if (!file) return;
@@ -143,27 +158,68 @@ function Form() {
             <input onChange={e=>setEmail(e.target.value)} name="user_email" value={email}placeholder="Email" type="text" required  />
             
 
-            <input onChange={e=>setName(e.target.value)} value={name} placeholder="Full Name" type="text"  />
+            <input onChange={e=>setName(e.target.value)} value={name} placeholder="Full Name" type="text" required />
            
 
             
             
-            <input id='phone_number' name='phone_number' value={number} type='tel' placeholder="Whatsapp Number" onChange={(e)=>setNumber(e.target.value)} />
+            <input id='phone_number' name='phone_number' value={number} type='tel' placeholder="Whatsapp Number" onChange={(e)=>setNumber(e.target.value)} required/>
 
             <input onChange={e=>setLocation(e.target.value)} name='location' value={location}placeholder="location" type="text" required />
             
-            <input onChange={e=>setChurch(e.target.value)} name='Church' value={Church}placeholder="What Church Do You Attend?" type="text" />
+            <input onChange={e=>setChurch(e.target.value)} name='Church' value={Church}placeholder="What Church Do You Attend?" type="text" required />
 
             {/*<textarea onChange={e=>setAbout(e.target.value)} name="message" placeholder="Tell Us About Yourself Briefly" type="text" className='sendMail__message' required cols="30" rows="5"/>*/}
             
+            <p>
+                <h4>Have you taken our Leadership Training Course(LTC)?</h4>
+                <div>
+                    Yes<input  name='radio' className='radioB' type="radio"  value={taken} onChange={(e)=>setTaken('Yes')}/><span/>
+                    No<input  name='radio' className='radioB' type="radio" value={taken} onChange={(e)=>setTaken('No')}/>
+                </div>
+            </p>
 
             <p>
-                <h4>Will You Be Available For All The Trainings?</h4>
+                <h4>If you have taken our Leadership Training Course(LTC), What month and year did you take the course?</h4>
                 <div>
-                    Yes<input  name='radio' type="radio"  value={available} onChange={(e)=>setAvailable('Yes')}/>
-                    No<input  name='radio' type="radio" value={available} onChange={(e)=>setAvailable('No')}/>
-                    </div>
-                </p>
+                    Month<p/><select value={month} onChange={e=>setMonth(e.target.value)}>
+                        <option></option>
+                        <option>January</option>
+                        <option>February</option>
+                        <option>March</option>
+                        <option>April</option>
+                        <option>May</option>
+                        <option>June</option>
+                        <option>July</option>
+                        <option>August</option>
+                        <option>September</option>
+                        <option>Octorber</option>
+                        <option>November</option>
+                        <option>December</option>
+                    </select>
+                    <span> </span>
+                    Year<p/><select value={year} onChange={e=>setYear(e.target.value)}>
+                        <option></option>
+                        <option>2022</option>
+                        <option>2021</option>
+                        <option>2020</option>
+                        <option>2019</option>
+                        <option>2018</option>
+                        <option>2017</option>
+                        <option>2016</option>
+                        <option>2015</option>
+                        <option>2014</option>
+                    </select>
+                </div>
+            </p>
+
+            <p>
+                <h4>Will You Be Available For All MTC Trainings?</h4>
+                <div>
+                    Yes<input  name='radi' className='radioB' type="radio"  value={available} onChange={(e)=>setAvailable('Yes')}/><span/>
+                    No<input  name='radi' className='radioB' type="radio" value={available} onChange={(e)=>setAvailable('No')}/>
+                </div>
+            </p>
             
             <p>
                 <h3>UPLOAD YOUR LETTER OF RECOMMENDATION. (If you are not a member of The Place of More Church)</h3>
